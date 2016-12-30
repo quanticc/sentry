37cf1cce93b297f7d1c7bc86be79083310b0a2d6
Ivan Abarca <ivan.abarca@usach.cl>
2016-12-30 16:28:32 -0300

    Add audit fields to Setting entity

--- src/main/java/top/quantic/sentry/domain/Setting.java
+++ src/main/java/top/quantic/sentry/domain/Setting.java
@@ -13,7 +13,7 @@ import java.util.Objects;
  */
 
 @Document(collection = "setting")
-public class Setting implements Serializable {
+public class Setting extends AbstractAuditingEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
