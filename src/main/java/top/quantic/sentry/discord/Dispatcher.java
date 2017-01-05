65348735aea4605ff329f4c6f1c2ef8657d74dd5
Ivan Abarca <ivan.abarca@usach.cl>
2017-01-04 23:59:33 -0300

    Handle parse errors and help correctly

--- src/main/java/top/quantic/sentry/discord/Dispatcher.java
+++ src/main/java/top/quantic/sentry/discord/Dispatcher.java
@@ -1,6 +1,7 @@
 package top.quantic.sentry.discord;
 
 import com.google.common.collect.Lists;
+import joptsimple.OptionException;
 import joptsimple.OptionParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,16 +22,12 @@ import top.quantic.sentry.discord.module.ListenerSupplier;
 import top.quantic.sentry.service.PermissionService;
 import top.quantic.sentry.service.SettingService;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static top.quantic.sentry.discord.util.DiscordLimiter.acquireDelete;
-import static top.quantic.sentry.discord.util.DiscordUtil.getRoles;
 import static top.quantic.sentry.discord.util.DiscordUtil.humanize;
 
 @Component
@@ -42,12 +39,15 @@ public class Dispatcher implements ListenerSupplier, IListener<MessageReceivedEv
     private final CommandRegistry commandRegistry;
     private final SettingService settingService;
     private final PermissionService permissionService;
+    private final Help help;
 
     @Autowired
-    public Dispatcher(CommandRegistry commandRegistry, SettingService settingService, PermissionService permissionService) {
+    public Dispatcher(CommandRegistry commandRegistry, SettingService settingService,
+                      PermissionService permissionService, Help help) {
         this.commandRegistry = commandRegistry;
         this.settingService = settingService;
         this.permissionService = permissionService;
+        this.help = help;
     }
 
     @Override
@@ -60,7 +60,7 @@ public class Dispatcher implements ListenerSupplier, IListener<MessageReceivedEv
         IMessage message = event.getMessage();
         String content = message.getContent();
         String guild = message.getChannel().isPrivate() ? Constants.ANY : message.getChannel().getGuild().getID();
-        List<String> prefixes = settingService.getPrefixes(guild);
+        Set<String> prefixes = settingService.getPrefixes(guild);
         Optional<String> prefix = prefixes.stream().filter(content::startsWith).findAny();
         if (prefix.isPresent()) {
             List<Command> commands = commandRegistry.getCommands(event.getClient());
@@ -75,19 +75,16 @@ public class Dispatcher implements ListenerSupplier, IListener<MessageReceivedEv
                 context.setPrefix(prefix.get());
 
                 // check permissions to execute the command
-                boolean allowed = true;
-                if (command.get().isSecured()) {
-                    allowed = permissionService.hasPermission(getRoles(message.getAuthor()),
-                        Operations.BOT_COMMAND_EXECUTE, command.get().getName());
-                }
-                boolean hasPermissions = event.getMessage().getChannel()
-                    .getModifiedPermissions(event.getMessage().getAuthor())
-                    .containsAll(command.get().getRequiredPermissions());
-                if (allowed && hasPermissions) {
+                boolean canExecute =
+                    (!command.get().isSecured() || permissionService.hasPermission(message, Operations.BOT_COMMAND_EXECUTE, command.get()))
+                        && message.getChannel().getModifiedPermissions(message.getAuthor()).containsAll(command.get().getRequiredPermissions());
+                if (canExecute) {
                     // clean up and parse arguments
                     String args = content.substring(content.indexOf(command.get().getName()) + command.get().getName().length());
                     args = args.startsWith(" ") ? args.split(" ", 2)[1] : null; // nullify if ran with no args
                     OptionParser parser = command.get().getParser();
+                    boolean parseError = false;
+                    boolean forHelp = false;
                     if (parser == null || parser.recognizedOptions().isEmpty()) {
                         if (args != null) {
                             // commands without parser but with args present, will delegate to onExecute
@@ -96,22 +93,36 @@ public class Dispatcher implements ListenerSupplier, IListener<MessageReceivedEv
                             context.setArgs(array);
                         }
                     } else {
-                        if (args != null) {
-                            int limit = command.get().getArgumentLimit();
-                            boolean unquote = !command.get().isPreserveQuotes();
-                            Map<String, String> aliases = command.get().getParameterAliases();
-                            String[] splitArgs = splitAndConvert(args, limit, unquote, aliases);
-                            context.setArgs(splitArgs);
-                            context.setOptionSet(parser.parse(splitArgs));
-                        } else {
-                            context.setOptionSet(parser.parse());
+                        try {
+                            if (args != null) {
+                                int limit = command.get().getArgumentLimit();
+                                boolean unquote = !command.get().isPreserveQuotes();
+                                Map<String, String> aliases = command.get().getParameterAliases();
+                                String[] splitArgs = splitAndConvert(args, limit, unquote, aliases);
+                                context.setArgs(splitArgs);
+                                context.setOptionSet(parser.parse(splitArgs));
+                            } else {
+                                context.setOptionSet(parser.parse());
+                            }
+                            forHelp = context.getOptionSet().has("help");
+                        } catch (OptionException e) {
+                            log.info("User {} executing command {}{} failed parsing: {}", humanize(message.getAuthor()),
+                                command.get().getName(), args == null ? "" : " with args: " + args, e.toString());
+                            help.replyWithHelp(command.get(), context, e.getMessage());
+                            parseError = true;
                         }
                     }
 
                     // finally, execute the command
                     log.info("User {} executing command {}{}", humanize(message.getAuthor()), command.get().getName(),
                         args == null ? "" : " with args: " + args);
-                    command.get().onExecute().accept(context);
+
+                    // intercept with help if parse failed or was explicitly requested
+                    if (forHelp) {
+                        help.replyWithHelp(command.get(), context);
+                    } else if (!parseError) {
+                        command.get().onExecute().accept(context);
+                    }
                     if (command.get().isDeleteRequest()) {
                         RequestBuffer.request(() -> {
                             try {
