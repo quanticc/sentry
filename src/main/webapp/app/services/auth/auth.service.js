ac80e3c9eca44ec86d5ad3ea083c101ff7b9f497
Ivan Abarca <ivan.abarca@usach.cl>
2017-01-25 01:56:12 -0300

    Fix an instance of a CSRF token issue

--- src/main/webapp/app/services/auth/auth.service.js
+++ src/main/webapp/app/services/auth/auth.service.js
@@ -68,9 +68,9 @@
                         storePreviousState($rootScope.toState.name, $rootScope.toStateParams);
 
                         // now, send them to the signin state so they can log in
-                        $state.go('accessdenied').then(function() {
+                        $state.go('accessdenied');/*.then(function() {
                             LoginService.open();
-                        });
+                        });*/ // Fix for some cases of wrong CSRF token
                     }
                 }
             }
