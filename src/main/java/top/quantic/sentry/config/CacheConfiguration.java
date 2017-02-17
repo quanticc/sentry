dbf208f3ef72698fa9854627c513384e8648c68c
Ivan Abarca <ivan.abarca@usach.cl>
2017-02-17 01:35:19 -0300

    Update CacheConfiguration

--- src/main/java/top/quantic/sentry/config/CacheConfiguration.java
+++ src/main/java/top/quantic/sentry/config/CacheConfiguration.java
@@ -1,18 +1,18 @@
 package top.quantic.sentry.config;
 
 
+import com.google.common.cache.CacheBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.support.NoOpCacheManager;
+import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PreDestroy;
-import javax.inject.Inject;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
@@ -22,29 +22,11 @@ public class CacheConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(CacheConfiguration.class);
 
-    @Inject
-    private CacheManager cacheManager;
-
-    @PreDestroy
-    public void destroy() {
-        log.info("Closing Cache Manager");
-    }
-
     @Bean
     public CacheManager cacheManager() {
-        log.debug("No cache");
-        cacheManager = new NoOpCacheManager();
-        return cacheManager;
+        log.debug("Creating Guava cache manager");
+        GuavaCacheManager guavaCacheManager = new GuavaCacheManager();
+        guavaCacheManager.setCacheBuilder(CacheBuilder.newBuilder().expireAfterAccess(3, TimeUnit.HOURS));
+        return guavaCacheManager;
     }
-
-//    @Bean
-//    public CacheManager cacheManager() {
-//        log.debug("Simple cache enabled");
-//        SimpleCacheManager manager = new SimpleCacheManager();
-//        manager.setCaches(Arrays.asList(
-//            new ConcurrentMapCache("permissions"),
-//            new ConcurrentMapCache("prefixes")));
-//        cacheManager = manager;
-//        return cacheManager;
-//    }
 }
