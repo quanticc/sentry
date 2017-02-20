c7af2692b8e497b2bc2e9bd7b916e6e791324e98
Ivan Abarca <ivan.abarca@usach.cl>
2017-02-20 15:47:36 -0300

    Give specific expiration values to each cache

--- src/main/java/top/quantic/sentry/config/CacheConfiguration.java
+++ src/main/java/top/quantic/sentry/config/CacheConfiguration.java
@@ -12,6 +12,8 @@ import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -25,8 +27,33 @@ public class CacheConfiguration {
     @Bean
     public CacheManager cacheManager() {
         log.debug("Creating Guava cache manager");
-        GuavaCacheManager guavaCacheManager = new GuavaCacheManager();
-        guavaCacheManager.setCacheBuilder(CacheBuilder.newBuilder().expireAfterAccess(30, TimeUnit.MINUTES));
+        CacheBuilder<Object, Object> thirtyMinuteBuilder = CacheBuilder.newBuilder().expireAfterAccess(30, TimeUnit.MINUTES).recordStats();
+        CacheBuilder<Object, Object> oneHourBuilder = CacheBuilder.newBuilder().expireAfterAccess(1, TimeUnit.HOURS).recordStats();
+        CacheBuilder<Object, Object> threeHoursBuilder = CacheBuilder.newBuilder().expireAfterAccess(3, TimeUnit.HOURS).recordStats();
+
+        Map<String, CacheBuilder<Object, Object>> builderMap = new LinkedHashMap<>();
+        builderMap.put("schedule", oneHourBuilder);
+        builderMap.put("team", threeHoursBuilder);
+        builderMap.put("roster", threeHoursBuilder);
+        builderMap.put("results", thirtyMinuteBuilder);
+        builderMap.put("legacyPlayer", threeHoursBuilder);
+        builderMap.put("player", threeHoursBuilder);
+        builderMap.put("banList", threeHoursBuilder);
+        builderMap.put("transactions", thirtyMinuteBuilder);
+
+        GuavaCacheManager guavaCacheManager = new GuavaCacheManager() {
+            @Override
+            protected com.google.common.cache.Cache<Object, Object> createNativeGuavaCache(String name) {
+                CacheBuilder<Object, Object> builder = builderMap.get(name);
+                if (builder != null) {
+                    log.debug("Creating cache for {}: {}", name, builder.toString());
+                    return builder.build();
+                }
+                log.debug("Creating fallback cache for {}", name);
+                return super.createNativeGuavaCache(name);
+            }
+        };
+        guavaCacheManager.setCacheBuilder(thirtyMinuteBuilder);
         return guavaCacheManager;
     }
 }
