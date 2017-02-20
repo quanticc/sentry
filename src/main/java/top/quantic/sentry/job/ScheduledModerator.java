c69a105055db79310016927775636326d2af6cf0
Ivan Abarca <ivan.abarca@usach.cl>
2017-02-20 05:12:32 -0300

    Allow multiple channels in scheduled pruning

--- src/main/java/top/quantic/sentry/job/ScheduledModerator.java
+++ src/main/java/top/quantic/sentry/job/ScheduledModerator.java
@@ -42,7 +42,7 @@ public class ScheduledModerator implements Job {
         JobDataMap jobDataMap = context.getMergedJobDataMap();
         String action = jobDataMap.getString("action");
         String bot = jobDataMap.getString("bot");
-        String channel = jobDataMap.getString("channel");
+        String channels = jobDataMap.getString("channels");
 
         IDiscordClient client;
 
@@ -65,85 +65,87 @@ public class ScheduledModerator implements Job {
         if (client == null) {
             throw new JobExecutionException("Parameter 'bot' is missing or invalid, it must map to a registered Bot");
         }
-        if (channel == null) {
-            throw new JobExecutionException("Parameter 'channel' must not be null, add it to the data map!");
+        if (channels == null) {
+            throw new JobExecutionException("Parameter 'channels' must not be null, add it to the data map!");
         }
 
         if ("purge".equals(action)) {
-            IChannel target = client.getChannelByID(channel);
-            if (target == null) {
-                throw new JobExecutionException("Could not find channel with ID " + channel);
-            }
+            for (String channel : channels.split(";|,")) {
+                IChannel target = client.getChannelByID(channel);
+                if (target == null) {
+                    throw new JobExecutionException("Could not find channel with ID " + channel);
+                }
 
-            String beforeTimex = jobDataMap.getString("before");
-            String afterTimex = jobDataMap.getString("after");
-            ZonedDateTime thisMinute = ZonedDateTime.now().truncatedTo(ChronoUnit.MINUTES);
-            ZonedDateTime before = beforeTimex == null ? thisMinute.minusDays(13) : parseTimeDate(beforeTimex);
-            ZonedDateTime after = afterTimex == null ? thisMinute.minusDays(14) : parseTimeDate(afterTimex);
-            if (before == null) {
-                throw new JobExecutionException("Could not parse a date from " + beforeTimex);
-            }
-            if (after == null) {
-                throw new JobExecutionException("Could not parse a date from " + afterTimex);
-            }
+                String beforeTimex = jobDataMap.getString("before");
+                String afterTimex = jobDataMap.getString("after");
+                ZonedDateTime thisMinute = ZonedDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+                ZonedDateTime before = beforeTimex == null ? thisMinute.minusDays(13) : parseTimeDate(beforeTimex);
+                ZonedDateTime after = afterTimex == null ? thisMinute.minusDays(14) : parseTimeDate(afterTimex);
+                if (before == null) {
+                    throw new JobExecutionException("Could not parse a date from " + beforeTimex);
+                }
+                if (after == null) {
+                    throw new JobExecutionException("Could not parse a date from " + afterTimex);
+                }
 
-            log.info("Preparing to purge channel {} from {} to {}", humanize(target),
-                after.toOffsetDateTime().toString(), before.toOffsetDateTime().toString());
+                log.info("Preparing to purge channel {} from {} to {}", humanize(target),
+                    after.toOffsetDateTime().toString(), before.toOffsetDateTime().toString());
 
-            MessageList messages = target.getMessages();
-            int capacity = messages.getCacheCapacity();
-            messages.setCacheCapacity(MessageList.UNLIMITED_CAPACITY);
+                MessageList messages = target.getMessages();
+                int capacity = messages.getCacheCapacity();
+                messages.setCacheCapacity(MessageList.UNLIMITED_CAPACITY);
 
-            // collect all offending messages
-            List<IMessage> toDelete = new ArrayList<>();
-            int i = 0;
-            while (true) {
-                try {
-                    IMessage msg = messages.get(i++);
-                    if (msg.getTimestamp().isAfter(before.toLocalDateTime())) {
-                        continue;
-                    }
-                    if (msg.getTimestamp().isBefore(after.toLocalDateTime())) {
-                        log.debug("Search interrupted after hitting date constraint");
+                // collect all offending messages
+                List<IMessage> toDelete = new ArrayList<>();
+                int i = 0;
+                while (true) {
+                    try {
+                        IMessage msg = messages.get(i++);
+                        if (msg.getTimestamp().isAfter(before.toLocalDateTime())) {
+                            continue;
+                        }
+                        if (msg.getTimestamp().isBefore(after.toLocalDateTime())) {
+                            log.debug("Search interrupted after hitting date constraint");
+                            break;
+                        }
+                        toDelete.add(msg);
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        // we reached the end apparently
+                        log.warn("Could not retrieve messages to delete: {}", e.getMessage());
                         break;
                     }
-                    toDelete.add(msg);
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    // we reached the end apparently
-                    log.warn("Could not retrieve messages to delete: {}", e.getMessage());
-                    break;
                 }
-            }
 
-            if (toDelete.size() > 1 && (before.isBefore(thisMinute.minusWeeks(2)) || after.isBefore(thisMinute.minusWeeks(2)))) {
-                throw new JobExecutionException("Cannot bulk delete messages before 2 weeks ago");
-            }
+                if (toDelete.size() > 1 && (before.isBefore(thisMinute.minusWeeks(2)) || after.isBefore(thisMinute.minusWeeks(2)))) {
+                    throw new JobExecutionException("Cannot bulk delete messages before 2 weeks ago");
+                }
 
-            log.info("Deleting {} after searching through {}", inflect(toDelete.size(), "message"), inflect(i, "message"));
+                log.info("Deleting {} after searching through {}", inflect(toDelete.size(), "message"), inflect(i, "message"));
 
-            // bulk delete requires at least 2 messages
-            CompletableFuture<Result<Integer>> future;
-            if (toDelete.size() == 1) {
-                future = CompletableFuture.supplyAsync(() -> deleteMessage(toDelete.get(0)).get());
-            } else {
-                future = CompletableFuture.supplyAsync(() -> deleteInBatch(target, toDelete));
-            }
-            future.whenComplete((result, error) -> {
-                if (result != null) {
-                    if (!result.isSuccessful()) {
-                        log.warn("Error: {}", result.getMessage());
-                    } else {
-                        if (!result.getContent().equals(toDelete.size())) {
-                            log.warn("Some messages could not be deleted: {} out of {}", result.getContent(), toDelete.size());
+                // bulk delete requires at least 2 messages
+                CompletableFuture<Result<Integer>> future;
+                if (toDelete.size() == 1) {
+                    future = CompletableFuture.supplyAsync(() -> deleteMessage(toDelete.get(0)).get());
+                } else {
+                    future = CompletableFuture.supplyAsync(() -> deleteInBatch(target, toDelete));
+                }
+                future.whenComplete((result, error) -> {
+                    if (result != null) {
+                        if (!result.isSuccessful()) {
+                            log.warn("Error: {}", result.getMessage());
+                        } else {
+                            if (!result.getContent().equals(toDelete.size())) {
+                                log.warn("Some messages could not be deleted: {} out of {}", result.getContent(), toDelete.size());
+                            }
+                            log.info("{}", (toDelete.size() == 0 ? "No messages were deleted" : "Deleted " + inflect(toDelete.size(), "message")));
+                            publisher.publishEvent(new ChannelPurgeEvent(target, toDelete));
                         }
-                        log.info("{}", (toDelete.size() == 0 ? "No messages were deleted" : "Deleted " + inflect(toDelete.size(), "message")));
-                        publisher.publishEvent(new ChannelPurgeEvent(target, toDelete));
                     }
-                }
-                if (error != null) {
-                    log.warn("Error while deleting messages");
-                }
-            }).thenRun(() -> messages.setCacheCapacity(capacity));
+                    if (error != null) {
+                        log.warn("Error while deleting messages");
+                    }
+                }).thenRun(() -> messages.setCacheCapacity(capacity));
+            }
         } else {
             log.warn("Unknown action: {}", action);
         }
