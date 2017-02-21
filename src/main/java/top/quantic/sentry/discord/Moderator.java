c524c241c5120a278ae39e653ba858ac832801dc
Ivan Abarca <ivan.abarca@usach.cl>
2017-02-21 16:57:01 -0300

    Add examples to delete command

--- src/main/java/top/quantic/sentry/discord/Moderator.java
+++ src/main/java/top/quantic/sentry/discord/Moderator.java
@@ -55,6 +55,7 @@ public class Moderator implements CommandSupplier {
         return CommandBuilder.of("delete")
             .describedAs("Delete messages on this channel")
             .in("Moderation")
+            .withExamples(deleteExamples())
             .parsedBy(parser)
             .secured()
             .onExecute(context -> {
@@ -254,5 +255,23 @@ public class Moderator implements CommandSupplier {
                     }).thenRun(() -> messages.setCacheCapacity(capacity));
                 }
             }).build();
+    }
+
+    private String deleteExamples() {
+        return "Usage: **delete** [**last** __number__] [**from** __user__] [**after** __timex__] [**before** __timex__] [**like** __content__] [**matching** __regex__] [**depth** __number__] [**include-this**] [**test**] [__user1__ [__user2__ ...]]\n\n" +
+            "This command let's you set a bunch of criteria, so let me explain this step by step:\n" +
+            "- Option **last** specifies the maximum number of messages to be deleted. For example, if you set 1 it will delete the most recent message matching your criteria. If you omit this it defaults to 100.\n" +
+            "- Option **from** lets you set the user that will have their messages deleted. It can be an ID, a name, a nickname or a mention.\n" +
+            "- Options **after** and **before** expect a temporal expression like \"2 hours ago\" or a date/time. Take note that Discord does not allow deleting messages older than 2 weeks in bulk and they must be deleted manually or with 'last 1' option.\n" +
+            "- Option **like** lets you specify a string (wrap within quotes if you need spaces) to delete all messages (until depth or limit is reached) that contain it.\n" +
+            "- Option **matching** lets you specify a regex (regular expression) to delete all messages (until depth or limit is reached) that match it.\n" +
+            "- Option **depth** lets you set the maximum number of messages searched. If omitted it defaults to 1000.\n" +
+            "- Option **include-this** makes your own command to be included in the search criteria, otherwise it will be ignored, even if it matches the given criteria.\n" +
+            "- Option **test** won't delete any message, instead it will PM you what would be deleted with the given criteria.\n" +
+            "- After all options, you can set additional users by ID, name, nickname or mention in case you want to delete messages from multiple users.\n\n" +
+            "To delete all messages in the past hour: `delete after \"1 hour ago\"` (see the quotes in order to use spaces)\n" +
+            "To delete last 10 messages from user named beepboop: `delete last 10 from beepboop` (no need to use mention)\n" +
+            "To delete last 100 messages containing the word 'human': `delete last 100 like human`\n" +
+            "To delete all messages older than 10 days: `delete before \"10 days ago\"` (uses default for last and depth)\n";
     }
 }
