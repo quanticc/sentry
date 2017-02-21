91f842d04209ba748a0199f497236cd21df09439
Ivan Abarca <ivan.abarca@usach.cl>
2017-02-21 18:25:26 -0300

    Filter banned events by guild

--- src/main/java/top/quantic/sentry/event/UserBannedEvent.java
+++ src/main/java/top/quantic/sentry/event/UserBannedEvent.java
@@ -2,13 +2,19 @@ package top.quantic.sentry.event;
 
 import com.vdurmont.emoji.Emoji;
 import com.vdurmont.emoji.EmojiManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import sx.blah.discord.api.internal.json.objects.EmbedObject;
 import sx.blah.discord.handle.impl.events.guild.member.UserBanEvent;
 
+import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class UserBannedEvent extends SentryEvent {
+
+    private static final Logger log = LoggerFactory.getLogger(UserBannedEvent.class);
 
     public UserBannedEvent(UserBanEvent event) {
         super(event);
@@ -29,8 +35,21 @@ public class UserBannedEvent extends SentryEvent {
 
     @Override
     public String asContent(Map<String, Object> dataMap) {
-        Emoji hammer = EmojiManager.getForAlias("hammer");
-        return hammer.getUnicode() + " " + getSource().getUser().getName();
+        String guildSpec = (String) dataMap.get("guilds");
+        List<String> guilds = null;
+        if (guildSpec != null) {
+            guilds = Arrays.asList(guildSpec.split(",|;"));
+        }
+        if (guilds == null) {
+            log.info("No guilds specified - Add 'guilds' to dataMap");
+            return null;
+        }
+        if (guilds.contains(getSource().getGuild().getID())) {
+            Emoji hammer = EmojiManager.getForAlias("hammer");
+            return hammer.getUnicode() + " " + getSource().getUser().getName();
+        } else {
+            return null;
+        }
     }
 
     @Override
