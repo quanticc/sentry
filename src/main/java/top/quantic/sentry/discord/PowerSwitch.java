2698d5489bf803b2d5bacac32ac619497005c08d
Ivan Abarca <ivan.abarca@usach.cl>
2017-02-22 21:39:02 -0300

    Add a command to issue close codes to ws

--- src/main/java/top/quantic/sentry/discord/PowerSwitch.java
+++ src/main/java/top/quantic/sentry/discord/PowerSwitch.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.api.IShard;
+import sx.blah.discord.api.internal.ShardImpl;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.util.DiscordException;
 import top.quantic.sentry.discord.core.Command;
@@ -11,9 +13,10 @@ import top.quantic.sentry.discord.core.CommandBuilder;
 import top.quantic.sentry.discord.core.CommandContext;
 import top.quantic.sentry.discord.module.CommandSupplier;
 
-import java.util.Collections;
 import java.util.List;
 
+import static java.util.Arrays.asList;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static top.quantic.sentry.config.Constants.INSTANCE_KEY;
 import static top.quantic.sentry.discord.util.DiscordUtil.*;
 
@@ -24,7 +27,38 @@ public class PowerSwitch implements CommandSupplier {
 
     @Override
     public List<Command> getCommands() {
-        return Collections.singletonList(logout());
+        return asList(logout(), close());
+    }
+
+    private Command close() {
+        return CommandBuilder.of("close")
+            .describedAs("Send a close code to the bot")
+            .in("Administrative")
+            .nonParsed()
+            .secured()
+            .onExecute(context -> {
+                IMessage message = context.getMessage();
+                String[] args = context.getArgs();
+                int code = 1000;
+                String reason = null;
+                if (args != null) {
+                    String[] array = args[0].split(" ", 2);
+                    if (array.length > 0) {
+                        code = Integer.parseInt(array[0]);
+                    }
+                    if (array.length > 1) {
+                        reason = array[1];
+                    }
+                }
+                try {
+                    SECONDS.sleep(3);
+                    for (IShard shard : message.getClient().getShards()) {
+                        ((ShardImpl) shard).ws.onWebSocketClose(code, reason);
+                    }
+                } catch (InterruptedException e) {
+                    log.warn("Interrupted!");
+                }
+            }).build();
     }
 
     private Command logout() {
